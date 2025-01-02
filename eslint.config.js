d771fd6ee666518db7d5333781271d08ce1962d8
Caleb Westaway <calebwestaway@icloud.com>
2025-01-02 13:14:21 +0000

    Update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -8,6 +8,7 @@ export default [
   { files: ["**/*.{js,mjs,cjs,ts}"] },
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
+  tseslint.configs.recommendedTypeChecked,
+  tseslint.configs.strictTypeChecked,
   eslintConfigPrettier,
 ];
