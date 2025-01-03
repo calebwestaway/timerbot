fd1c7ce7c83694987cbbf41400778777f431144a
Caleb Westaway <calebwestaway@icloud.com>
2025-01-03 02:41:54 +0000

    Implement CLI

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,52 @@
-import { Client, Events, GatewayIntentBits } from "discord.js";
+import * as readline from "node:readline/promises";
+import {
+  ChannelType,
+  Client,
+  Events,
+  GatewayIntentBits,
+  TextChannel,
+} from "discord.js";
 import "dotenv/config";
+import { exit } from "node:process";
 
-const client: Client = new Client({ intents: GatewayIntentBits.Guilds });
+const rl: readline.Interface = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const client: Client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
+});
 
 client.once(Events.ClientReady, (readyClient) => {
   console.log("Ready! Logged in as " + readyClient.user.tag);
+
+  rl.prompt();
+
+  rl.on("line", (input) => {
+    const command: string = input.trim();
+    const commandSplit: string[] =
+      command
+        .match(/(?:[^\s"]+|"([^"]*)")+/g)
+        ?.map((s) => s.replace(/"/g, "")) || [];
+    switch (commandSplit[0]) {
+      case "exit":
+        console.log("Exiting");
+        client.destroy();
+        rl.close();
+        exit(0);
+        break;
+      case "help":
+        console.log("Commands:");
+        console.log("  exit: Exit the program");
+        console.log("  help: Show this help message");
+        break;
+      default:
+        console.log(`Unknown command: ${command}. Type "help" for help`);
+        break;
+    }
+    rl.prompt();
+  });
 });
 
 client.login(process.env.TOKEN);
